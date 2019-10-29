3b2ffa8a1d5d155c3f7c8c75834678f3774a1748
Kaelan Khan <32547155+KaelanKhan@users.noreply.github.com>
2019-10-29 11:57:14 -0400

    Created blackjack

---
+++ simplified21KaelanK/simplified21KaelanK/simplified21Form.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace simplified21KaelanK
+{
+    partial class frmSimplified21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBlackjack = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.lblCard1 = new System.Windows.Forms.Label();
+            this.lblCard2 = new System.Windows.Forms.Label();
+            this.lblCard3 = new System.Windows.Forms.Label();
+            this.lblDealerCard1 = new System.Windows.Forms.Label();
+            this.lblDealerCard2 = new System.Windows.Forms.Label();
+            this.lblDealerCard3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBlackjack
+            // 
+            this.lblBlackjack.AutoSize = true;
+            this.lblBlackjack.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlackjack.Location = new System.Drawing.Point(124, 114);
+            this.lblBlackjack.Name = "lblBlackjack";
+            this.lblBlackjack.Size = new System.Drawing.Size(407, 73);
+            this.lblBlackjack.TabIndex = 0;
+            this.lblBlackjack.Text = "BLACKJACK";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(277, 215);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 39);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Play";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(37, 377);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(96, 39);
+            this.btnHit.TabIndex = 2;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // btnStay
+            // 
+            this.btnStay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStay.Location = new System.Drawing.Point(539, 377);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(96, 39);
+            this.btnStay.TabIndex = 3;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            // 
+            // lblCard1
+            // 
+            this.lblCard1.AutoSize = true;
+            this.lblCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard1.Location = new System.Drawing.Point(178, 388);
+            this.lblCard1.Name = "lblCard1";
+            this.lblCard1.Size = new System.Drawing.Size(52, 20);
+            this.lblCard1.TabIndex = 5;
+            this.lblCard1.Text = "Card1";
+            // 
+            // lblCard2
+            // 
+            this.lblCard2.AutoSize = true;
+            this.lblCard2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard2.Location = new System.Drawing.Point(302, 388);
+            this.lblCard2.Name = "lblCard2";
+            this.lblCard2.Size = new System.Drawing.Size(52, 20);
+            this.lblCard2.TabIndex = 6;
+            this.lblCard2.Text = "Card2";
+            // 
+            // lblCard3
+            // 
+            this.lblCard3.AutoSize = true;
+            this.lblCard3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCard3.Location = new System.Drawing.Point(424, 388);
+            this.lblCard3.Name = "lblCard3";
+            this.lblCard3.Size = new System.Drawing.Size(52, 20);
+            this.lblCard3.TabIndex = 7;
+            this.lblCard3.Text = "Card3";
+            // 
+            // lblDealerCard1
+            // 
+            this.lblDealerCard1.AutoSize = true;
+            this.lblDealerCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard1.Location = new System.Drawing.Point(133, 46);
+            this.lblDealerCard1.Name = "lblDealerCard1";
+            this.lblDealerCard1.Size = new System.Drawing.Size(99, 20);
+            this.lblDealerCard1.TabIndex = 8;
+            this.lblDealerCard1.Text = "DealerCard1";
+            // 
+            // lblDealerCard2
+            // 
+            this.lblDealerCard2.AutoSize = true;
+            this.lblDealerCard2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard2.Location = new System.Drawing.Point(283, 46);
+            this.lblDealerCard2.Name = "lblDealerCard2";
+            this.lblDealerCard2.Size = new System.Drawing.Size(99, 20);
+            this.lblDealerCard2.TabIndex = 9;
+            this.lblDealerCard2.Text = "DealerCard2\r\n";
+            // 
+            // lblDealerCard3
+            // 
+            this.lblDealerCard3.AutoSize = true;
+            this.lblDealerCard3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard3.Location = new System.Drawing.Point(441, 46);
+            this.lblDealerCard3.Name = "lblDealerCard3";
+            this.lblDealerCard3.Size = new System.Drawing.Size(99, 20);
+            this.lblDealerCard3.TabIndex = 10;
+            this.lblDealerCard3.Text = "DealerCard3\n";
+            // 
+            // frmSimplified21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 450);
+            this.Controls.Add(this.lblDealerCard3);
+            this.Controls.Add(this.lblDealerCard2);
+            this.Controls.Add(this.lblDealerCard1);
+            this.Controls.Add(this.lblCard3);
+            this.Controls.Add(this.lblCard2);
+            this.Controls.Add(this.lblCard1);
+            this.Controls.Add(this.btnStay);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblBlackjack);
+            this.Name = "frmSimplified21";
+            this.Text = "Simplified 21 by Kaelan K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBlackjack;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Label lblCard1;
+        private System.Windows.Forms.Label lblCard2;
+        private System.Windows.Forms.Label lblCard3;
+        private System.Windows.Forms.Label lblDealerCard1;
+        private System.Windows.Forms.Label lblDealerCard2;
+        private System.Windows.Forms.Label lblDealerCard3;
+    }
+}
+
---
+++ simplified21KaelanK/simplified21KaelanK/simplified21Form.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simplified21KaelanK
+{
+    public partial class frmSimplified21 : Form
+    {
+        public frmSimplified21()
+        {
+            InitializeComponent();
+
+            // hide labels at beginning
+            this.lblCard1.Hide();
+            this.lblCard2.Hide();
+            this.lblCard3.Hide();
+            this.lblDealerCard1.Hide();
+            this.lblDealerCard2.Hide();
+            this.lblDealerCard3.Hide();
+            this.btnHit.Hide();
+            this.btnStay.Hide();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // Show the labels after button clicked
+            this.lblCard1.Show();
+            this.lblCard2.Show();
+            this.lblCard3.Show();
+            this.lblDealerCard1.Show();
+            this.lblDealerCard2.Show();
+            this.lblDealerCard3.Show();
+            this.btnHit.Show();
+            this.btnStay.Show();
+            // Hide other labels and button
+            this.lblBlackjack.Hide();
+            this.btnStart.Hide();
+        }
+    }
+}
